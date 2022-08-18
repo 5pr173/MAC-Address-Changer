0972c9d3635ad42a2828a6577ae4d796a68f7d4f
5pr173 <5pr173@protonmail.com>
2022-08-18 14:25:32 -0700

    First Release
    Program is user friendly and is read for release.

--- mac_changer.py
+++ mac_changer.py
@@ -1,7 +1,34 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
 
-subprocess.call("ifconfig wlx984827e68cf7 down", shell=True)
-subprocess.call("ifconfig wlx984827e68cf7 hw ether 00:11:22:33:44:66", shell=True)
-subprocess.call("ifconfig wlx984827e68cf7 up", shell=True)
+parser = optparse.OptionParser()
+
+parser.add_option('-r', '--run', dest='mode', help='Select the mode to run the script in: command or terminal')
+parser.add_option('-i', '--interface', dest='interface', help='The interface of the device you want to change the MAC address of.')
+parser.add_option('-m', '--mac', dest='new_mac', help='The MAC address that you want to be your new MAC address.')
+
+(options, args) = parser.parse_args()
+
+if options.mode == 'command':
+    interface_command = options.interface
+    new_mac_command = options.new_mac
+
+    print ('[+] Changin the MAC address for ' + interface_command + ' to ' + new_mac_command)
+
+    subprocess.call(['sudo', 'ifconfig', interface_command, 'down'])
+    subprocess.call(['sudo', 'ifconfig', interface_command, 'hw', 'ether', new_mac_command])
+    subprocess.call(['sudo', 'ifconfig', interface_command, 'up'])
+
+elif options.mode == 'terminal':
+    interface_terminal = input('Interface > ')
+    new_mac_terminal = input('MAC address > ')
+
+    print('[+] Changing the MAC address for ' + interface_terminal + ' to ' + new_mac_terminal)
+
+    subprocess.call(['sudo', 'ifconfig', interface_terminal, 'down'])
+    subprocess.call(['sudo', 'ifconfig', interface_terminal, 'hw', 'ether', new_mac_terminal])
+    subprocess.call(['sudo', 'ifconfig', interface_terminal, 'up'])
+else:
+    print('Please the --help command to see how to run this program.')
